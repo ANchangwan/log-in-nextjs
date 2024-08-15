24df4c58915b5645b8a7fd7c08f0752a955a2c17
안창완 <ssdii66@gmail.com>
2024-08-15 12:00:40 +0900

    수정

--- app/(home)/action.ts
+++ app/(home)/action.ts
@@ -3,10 +3,10 @@
 import { error } from "console";
 
 export default async function FormAction(prevData: any, FormData: FormData) {
-  await new Promise((response) => setTimeout(response, 4000));
+  await new Promise((response) => setTimeout(response, 2000));
   const password = FormData.get("password");
 
-  if (password !== "1234") {
+  if (password !== "12345") {
     return {
       errors: ["Wrong Password!!"],
     };

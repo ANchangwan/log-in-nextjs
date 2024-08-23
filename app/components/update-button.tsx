a0a1dc75b69f456522075f10c67e20e5b3fcce77
안창완 <ssdii66@gmail.com>
2024-08-23 17:58:03 +0900

    Tweets 상세페이지, 로그인한 유저의 글은  수정 삭제 가능2

--- app/components/update-button.tsx
+++ app/components/update-button.tsx
@@ -1,7 +1,7 @@
 export default function UpdateButton() {
   return (
     <form>
-      <button className="px-4 py-2 rounded-lg bg-blue-500 border-none text-white">
+      <button className=" px-4 py-2 rounded-lg bg-blue-500 border-none text-white">
         수정하기
       </button>
     </form>

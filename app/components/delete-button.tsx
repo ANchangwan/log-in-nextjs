ffaa41ef11a638d8b9d570bed656743ea09d45c6
안창완 <ssdii66@gmail.com>
2024-08-24 16:28:48 +0900

    delete 구현

--- app/components/delete-button.tsx
+++ app/components/delete-button.tsx
@@ -1,6 +1,21 @@
-export default function DeleteButton() {
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+export default async function DeleteButton() {
+  const session = await getSession();
+  const onDelete = async (formData: FormData) => {
+    "use server";
+    const tweets = await db.tweet.delete({
+      where: {
+        id: session.id,
+      },
+    });
+    console.log(tweets);
+    redirect("/");
+  };
   return (
-    <form>
+    <form action={onDelete}>
       <button className="px-4 py-2 rounded-lg bg-red-500 border-none text-white">
         삭제하기
       </button>
